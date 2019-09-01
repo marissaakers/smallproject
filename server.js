549c31791bdd54a3969aad3e9e09bca583ec2ffb
Ryan Stern <ryanstern@ryans-mbp.attlocal.net>
2019-09-01 00:29:34 -0400

    Added all user functionality / schema

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const userRouter = require('./routes/user')
+
+// Connecting to mongo database
+mongoose.connect('mongodb+srv://ryan:contactmanagerpw@contact-manager-ptzoy.mongodb.net/test?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/user', userRouter)
 
 module.exports = app
