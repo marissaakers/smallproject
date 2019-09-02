78b7782db0c6fd2459aa0b638bf95eabd05f8a8d
Ryan Stern <ryanstern@ryans-mbp.attlocal.net>
2019-09-02 17:58:28 -0400

    added most contact functionality, usage of jwt

---
+++ models/contactModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const contactSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  number: {
+    type: Number,
+    required: true,
+    unique: false
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: false
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
---
+++ routes/contact.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = express()
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+
+const Contact = require('../models/contactModel')
+
+router.get('/', function(req, res) {
+  const decoded = jwt.decode(req.headers.authorization.split(" ")[1])
+
+  Contact.find({owner: decoded.userId})
+  .populate('owner', 'username')
+  .exec()
+  .then(contacts => {
+    res.status(200).json(contacts)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+router.get('/search', function(req, res) {
+  Contact.findOne({name: req.body.searchName})
+  .exec()
+  .then(contact => {
+    res.status(200).json(contact)
+  })
+  .catch(err => {
+    res.status(500).json(err)
+  })
+})
+
+router.post('/create-contact', function(req, res) {
+  const decoded = jwt.decode(req.headers.authorization.split(" ")[1])
+
+  const contact = new Contact({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    number: req.body.number,
+    email: req.body.email,
+    owner: decoded.userId
+  })
+
+  Contact.create(contact, (err, contact) => {
+    if(err) {
+      res.status(500).json(err)
+    } else {
+      res.status(201).json(contact)
+    }
+  })
+})
+
+module.exports = router
