cac0290f3baaf917dc331a166ec34fbbb6803cef
Ryan Stern <ryanstern@ryans-mbp.attlocal.net>
2019-09-03 11:40:44 -0400

    commented out the middleware file

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 
+// Exports a function that will verify the jwt being passed and return the parsed data
 module.exports = (req, res, next) => {
   try{
     const decoded = jwt.verify(req.headers.authorization.split(" ")[1], 'contactmanagerpw')
