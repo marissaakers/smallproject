a5fa3daa6e5341284287f9444437c0c0e6d8e392
Ryan Stern <ryanstern@Ryans-MacBook-Pro.local>
2019-09-11 17:41:53 -0400

    barebones of signup page

--- client/src/pages/users.signup.js
+++ client/src/pages/users.signup.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Form, FormControl, FormGroup, FormLabel } from 'react-bootstrap/'
+import { Button } from 'react-bootstrap/'
 
 class SignUp extends Component {
   constructor(props) {
@@ -13,6 +15,22 @@ class SignUp extends Component {
     return(
       <div>
         <h1>Sign Up</h1>
+        <div>
+        <Form>
+          <FormGroup>
+            <FormLabel>Username</FormLabel>
+            <FormControl type="username" placeholder="username" />
+          </FormGroup>
+
+          <FormGroup controlId="formBasicPassword">
+            <FormLabel>Password</FormLabel>
+            <Form.Control type="password" placeholder="password" />
+          </FormGroup>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        </div>
       </div>
     )
   }
