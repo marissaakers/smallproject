9145868216ab1df3ce444ae2ef0de2b409b22065
baldwinsuen <suenbaldwin@gmail.com>
2019-09-16 17:24:39 -0400

    created login page and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './pages/landing'
 import SignUp from './pages/users.signup'
+import Login from './pages/users.login'
+import Dashboard from './pages/dashboard'
 import './App.css';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Landing} />
           <Route path='/users/signup' component={SignUp} />
+          <Route path='/users/login' component={Login} />
         </Switch>
       </Router>
     </div>
