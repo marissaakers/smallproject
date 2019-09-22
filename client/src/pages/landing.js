a536055436a5a205ba504976fe8fd9f785494b7f
Marissa Akers <Marissa@Marissas-MacBook-Pro.local>
2019-09-21 20:02:59 -0400

    landing page draft

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import { Link }from 'react-router-dom'
+import logo from './rybamatransparent.png'; // Tell Webpack this JS file uses this image
+import signuplogo from './signupdim.png';
+import loginlogo from './logindim.png';
+import signuplit from './signup_lit.png'
+import loginlit from './signuplit.png'
+
+console.log(logo);
+console.log(loginlogo);
+console.log(signuplogo);
+console.log(signuplit);
+console.log(loginlit);
 
 class Landing extends Component {
   render() {
     return(
       <div style={styling.mainDiv} >
-      <h1 style={{margin: '5%'}} >RYBAMA</h1>
+
+      <div> <img src={logo} width = "800"/> </div>
+
       <div style={styling.buttonDiv}>
-        <Link to="/users/signup"><Button style={styling.signup}>Sign Up</Button></Link> 
-        <Link to='/users/login'><Button style={styling.buttons}>Login</Button></Link>
+        <a href="#" id="image"><Link to="/users/signup"><input type="image" src={signuplogo} onMouseOver="this.src={signuplit}" onMouseOut="this.src='./signupdim.png'"  width = "700"/></Link></a>
+        <Link to="/users/login"><input type="image" src={loginlogo} width = "700"/></Link>
       </div>
     </div>
     )
@@ -18,20 +31,22 @@ class Landing extends Component {
 
 const styling = {
   mainDiv: {
+    background: '#200c4c',
     textAlign: 'center'
   },
   buttons: {
-    margin: '25%',
-    padding: '40%'
+
+
   },
   signup: {
-    
+
     marginRight: '30%'
   },
   buttonDiv: {
-    display: 'flex',
+
+
     justifyContent: 'center',
-    // border: '1px solid black'
+
   }
 }
 
