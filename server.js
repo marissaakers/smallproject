6617845de3267a07f72ad3d333c538afe7481c89
Ryan Stern <ryanstern03@gmail.com>
2019-09-22 21:58:13 -0400

    stage

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // Allows front-end and api to talk to eachother
 app.use(cors())
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/users', userRouter)
 app.use('/contacts', contactRouter)
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 
