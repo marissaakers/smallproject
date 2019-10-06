e69c09114bb59940396fa6acc17a814bc4cc9561
Ryan Stern <ryanstern03@gmail.com>
2019-10-06 11:32:05 -0400

    im tired

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path');
 const cors = require('cors')
-const PORT =  process.env.PORT  || 5000
+const PORT =  process.env.PORT  || 8080
 const userRouter = require('./routes/user')
 const contactRouter = require('./routes/contact')
 
@@ -25,6 +25,7 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/users', userRouter)
 app.use('/contacts', contactRouter)
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
